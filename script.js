4f64de23e348ac277c55bcb62448be934ab147ed
Petar Ivanov <petar_1v@abv.bg>
2018-03-03 17:51:14 +0200

    bbb

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function click(arg){
         if (this.readyState == 4 && this.status == 200){
             data = JSON.parse(this.responseText);
             document.getElementById("tit").innerHTML = data[arg]["name"];
-            document.getElementById("txt").innerHTML = data[arg]["text"]
+            document.getElementById("txt").innerHTML = data[arg]["text"];
+            //document.getElementById("image").setAttribute("src", data[arg]["image"]);
         }
     };
     xhttp.open("GET","data.json",true);
